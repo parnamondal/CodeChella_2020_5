aa0ee41323dd4292a0329911c9d7f5beb2ea90e5
arcsoftech <arihant_Chhajed@yahoo.in>
2020-11-21 11:47:32 -0600

    Twitter API search query implemneted

---
+++ server/routes/twitterAPI.js
@@ -0,0 +1,48 @@
+/* jshint esversion: 6 */
+// Author:Arihant Chhajed
+// Description:This script is to define route for accesing twitter api
+
+'use strict';
+
+/* Dependency injection and initialization */
+
+const express = require('express');
+const Twitter = require('twitter');
+const router = express.Router(); //eslint-disable-line
+
+const AUTH_PARAM = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+const client = new Twitter(AUTH_PARAM);
+
+
+const getTweets = async (SearchParam,count) => {
+    const params = {
+        q: SearchParam,
+        count
+    };
+
+    return new Promise((resolve, reject) => {
+        client.get('search/tweets.json', params, (error, tweets, response) => {
+            if (!error) {
+                resolve(tweets);
+            }
+            else {
+                console.log(error);
+                reject(error)
+            }
+        });
+
+    })
+
+}
+router.get("/getTweets/:SearchParam/:count", async (req, res) => {
+    console.log(req.params)
+    const tweets = await getTweets(req.params.SearchParam,req.params.count);
+    res.send(tweets);
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('./config/express.js')
  
 // Use env port or default
 const port = process.env.PORT || 5000;
-
 const app = express.init()
+require("dotenv").config();
+app.use('/twitter',require("./routes/twitterAPI"))
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
+
